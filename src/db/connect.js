db22d66fbb2483ad61d00ede393478ba0019a79a
Arthur16800 <arthur08coutinho@gmail.com>
2025-03-10 11:00:18 -0300

    Atualização do banco de dados com VIEW

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host:'localhost',
+    host:'10.89.234.161',
     user:'alunods',
     password:'senai@604',
     database:'vio_arthur'
